2ad770b8cef779ecdb3a3fe075619b04432d315f
yzcode <codeoforyz@gmail.com>
2016-02-07 18:40:00 +0800

    event trace chart beta

--- src/components/Content-event-trace-chart.vue
+++ src/components/Content-event-trace-chart.vue
@@ -15,55 +15,79 @@
 import contentFrame from './Content-frame.vue'
 var echarts = require('echarts')
 // test content
+var keyword = ['南京 宝马 警方', '南京 宝马 肇事', '南京 宝马 嫌疑人', '南京 宝马 超速', '南京 宝马 驾驶证', '南京 宝马 死亡', '南京 宝马 事故', '南京 宝马', '南京 宝马 无照驾驶', '南京 宝马', '南京 宝马', '南京 宝马 毒驾']
 var option = {
   tooltip: {
-    trigger: 'axis'
-  },
-  legend: {
-    data: ['蒸发量', '降水量']
+    trigger: 'axis',
+    formatter: function (params, ticket, callback) {
+      var tip_str = params[0].name + '<br/>'
+      for (var i in params) {
+        tip_str += params[i].seriesName + ': ' + params[i].value + '<br/>'
+      }
+      tip_str += '关键词' + ': ' + keyword[params[0].dataIndex] + '<br/>'
+      return tip_str
+    }
   },
   toolbox: {
     show: true,
     feature: {
-      mark: {
-        show: true
-      },
-      dataView: {
-        show: true,
-        readOnly: false
-      },
-      magicType: {
-        show: true,
-        type: ['line', 'bar']
-      },
-      restore: {
-        show: true
-      },
-      saveAsImage: {
-        show: true
-      }
+      mark: {show: true},
+      dataView: {show: true, readOnly: false},
+      magicType: {show: true, type: ['line', 'bar']},
+      restore: {show: true},
+      saveAsImage: {show: true}
     }
   },
   calculable: true,
-  xAxis: [{
-    type: 'category',
-    data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-  }],
-  yAxis: [{
-    type: 'value',
-    splitArea: {
-      show: true
+  legend: {
+    data: ['负面情感指数', '正面情感指数', '热度指数']
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['1号', '2号', '3号', '4号', '5号', '6号', '7号', '8号', '9号', '10号', '11号', '12号']
     }
-  }],
-  series: [{
-    name: '蒸发量',
-    type: 'bar',
-    data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-  }, {
-    name: '降水量',
-    type: 'bar',
-    data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-  }]
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: '情感指数',
+      min: 0,
+      max: 250,
+      interval: 50,
+      axisLabel: {
+        formatter: '{value}'
+      }
+    },
+    {
+      type: 'value',
+      name: '热度指数',
+      min: 0,
+      max: 25,
+      interval: 5,
+      axisLabel: {
+        formatter: '{value}'
+      }
+    }
+  ],
+  series: [
+    {
+      name: '负面情感指数',
+      type: 'bar',
+      data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+    },
+    {
+      name: '正面情感指数',
+      type: 'bar',
+      data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+    },
+    {
+      name: '热度指数',
+      type: 'line',
+      yAxisIndex: 1,
+      data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+    }
+  ]
 }
 // test content end
 export default {
